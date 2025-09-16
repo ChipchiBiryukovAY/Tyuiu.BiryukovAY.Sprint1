a708e87906487101000bfff0c8a42408d045a8a0
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-16 12:26:03 +0700

    Add project files.

---
+++ Tyuiu.BiryukovAY.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BiryukovAY.Sprint1.Task0.V0.Lib;
+
+//ЗАДАНИЕ
+//Написать программу, которая вычисляет выражение 10 / (2 + 3) и печает результат на экране.
+namespace Tyuiu.BiryukovAY.Sprint1.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Бирюков А. Ю. | ПКТб-25-1";
+            //Длина строки 75 символов
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в #C                                       *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #0                                                             *");
+            Console.WriteLine("* Выполнил: Бирюков Алексей Юрьевич | ПКТб-25-1                          *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 10 / (2 + 3)           *");
+            Console.WriteLine("* и печает результат на экране.                                          *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* 10 / (2 + 3)                                                           *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            //Метод Calculate находится в библиотеке Tyuiu.BiryukovAY.Sprint1.Task0.V0.Lib
+            //в классе DataService
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Tyuiu.BiryukovAY.Sprint1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BiryukovAY.Sprint1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BiryukovAY.Sprint1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b05f046-5645-43a0-b62e-f54a9783901d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
