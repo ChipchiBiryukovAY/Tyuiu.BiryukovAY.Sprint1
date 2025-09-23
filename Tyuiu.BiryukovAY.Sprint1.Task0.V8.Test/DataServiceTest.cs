bb2b97ce985841c7087e5b6f9d940b94de5630ae
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-23 16:50:30 +0700

    Add project files.

---
+++ Tyuiu.BiryukovAY.Sprint1.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BiryukovAY.Sprint1.Task0.V8.Lib;
+
+namespace Tyuiu.BiryukovAY.Sprint1.Task0.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.BiryukovAY.Sprint1.Task0.V8/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Tyuiu.BiryukovAY.Sprint1.Task0.V8.Lib;
+
+//ЗАДАНИЕ
+//Написать программу, которая вычисляет выражение 10 / 2 / 4 и печает результат на экране.
+namespace Tyuiu.BiryukovAY.Sprint1.Task0.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Бирюков А. Ю. | ПКТб-25-1";
+            //Длина строки 75 символов
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в #C                                       *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #0                                                             *");
+            Console.WriteLine("* Выполнил: Бирюков Алексей Юрьевич | ПКТб-25-1                          *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 15 / 2 / 4             *");
+            Console.WriteLine("* и печает результат на экране.                                          *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* 15 / 2 / 4                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            //Метод Calculate находится в библиотеке Tyuiu.BiryukovAY.Sprint1.Task0.V8.Lib
+            //в классе DataService
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
