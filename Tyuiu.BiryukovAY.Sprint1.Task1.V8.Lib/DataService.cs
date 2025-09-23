873f17b1a52b2a5d8220d4e35273dd17cdfc29c0
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-23 21:58:25 +0700

    Task1

---
+++ Tyuiu.BiryukovAY.Sprint1.Task1.V8.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BiryukovAY.Sprint1.Task1.V8.Lib
+{
+    public class DataService : ISprint1Task1V8
+    {
+        public double Calculate(double y, double x)
+        {
+            return (x * Math.PI) / y;
+        }
+    }
+}
---
+++ Tyuiu.BiryukovAY.Sprint1.Task1.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BiryukovAY.Sprint1.Task1.V8.Lib;
+namespace Tyuiu.BiryukovAY.Sprint1.Task1.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double a = 2.0;
+            var res = ds.Calculate(a, y);
+            Assert.AreEqual(6.283185307179586, res);
+
+        }
+    }
+}
