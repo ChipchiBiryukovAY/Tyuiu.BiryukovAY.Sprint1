d35a4c9d9146486354604012b79a1218e8d85cf7
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-24 22:21:42 +0700

    Task5

--- Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib/DataService.cs
+++ Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib/DataService.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib
 {
-    public class DataService : ISprint1Task5V2
+    public class TemperatureConverter : ISprint1Task5V2
     {
         public int FahrenheitToCelsius(double fahrenheit)
         {
