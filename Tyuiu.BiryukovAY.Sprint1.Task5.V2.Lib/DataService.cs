7f53d09911d90b4b70829162f09a86433a766762
Алексей Бирюков <ChipichiX@yandex.ru>
2025-09-24 22:18:50 +0700

    Task5

--- Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib/DataService.cs
+++ Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using System;
 
 namespace Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib
 {
@@ -7,7 +8,7 @@ namespace Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib
         public int FahrenheitToCelsius(double fahrenheit)
         {
             // Формула преобразования: C = (F - 32) * 5/9
-            double celsius = (fahrenheit - 32) * 5 / 9;
+            double celsius = (fahrenheit - 32) * 5.0 / 9.0;
 
             // Приводим к целому с помощью Convert
             return Convert.ToInt32(celsius);
