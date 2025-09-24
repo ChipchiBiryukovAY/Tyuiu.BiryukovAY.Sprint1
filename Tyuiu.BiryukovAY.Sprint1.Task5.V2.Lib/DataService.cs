0d83cf19e560765a9a335186303134b9344698df
ChipchiBiryukovAY <chipichix@yandex.ru>
2025-09-24 21:59:44 +0500

    Add files via upload

---
+++ Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using System;
+
+
+namespace Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib
+{
+    public class DataService : ISprint1Task5V2
+    {
+        public int FahrenheitToCelsius(double fahrenheit)
+        {
+            return Convert.ToInt32((fahrenheit - 32) * 5 / 9);
+        }
+    }
+}
