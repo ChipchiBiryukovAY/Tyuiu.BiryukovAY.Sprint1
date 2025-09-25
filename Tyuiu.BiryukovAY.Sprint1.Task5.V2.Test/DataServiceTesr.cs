efbb92e450661b066792b85f17e8a89b68db4eb6
ChipchiBiryukovAY <chipichix@yandex.ru>
2025-09-25 22:04:23 +0500

    Add files via upload

---
+++ Tyuiu.BiryukovAY.Sprint1.Task5.V2.Test/DataServiceTesr.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BiryukovAY.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.BiryukovAY.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class TemperatureConverterTests
+    {
+        [TestMethod]
+        public void FahrenheitToCelsius_32F_Returns0C()
+        {
+            DataService converter = new DataService();
+
+            int result = converter.FahrenheitToCelsius(32);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
